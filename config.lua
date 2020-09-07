987990146f64bea01dfdf8fee8735726f77da291
Phaos ¦ soahP#9050 <70765148+Phaos9050@users.noreply.github.com>
2020-09-07 14:38:40 +0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,88 @@
+Config = {}
+
+Config.vESX = 'esx:getMiSharedCheoObject'
+Config.EsxAmbulance = 'esx_ambulancejob:revive'
+
+Config.Command = "cuuthuong"
+Config.bacSiOnline = 0
+Config.Money = 200
+Config.jobName = 'ambulance'
+
+spawnRadius = 200
+drivingStyle = 1074528293
+CuuNguoiTrenXe = false
+
+BotBacSi = {
+
+        [1] = {model = "s_m_m_doctor_01", vehicle = 'ambulance', colour = 111,
+                ['lines'] = {
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                }
+        },
+
+        [2] = {model = "s_m_m_doctor_01", vehicle = 'ambulance', colour = 118, 
+                ['lines'] = {
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                }
+        },
+                                                                        
+        [3] = {model = "s_m_m_doctor_01", vehicle = 'ambulance', colour = 118, 
+                ['lines'] = {
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                }
+        },
+                                                                        
+        [4] = {model = "s_m_m_doctor_01", vehicle = 'ambulance', colour = 118, 
+                ['lines'] = {
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                        "Bạn đã được cứu.",
+                        "Hãy DONATE để giúp bọn tôi duy trì server nhé.",
+                }
+        },
+
+}
+
+Config.locale = {
+        a = 'Bạn không cần cứu thương',
+        b = 'Một cô y tá đã được gửi đến vị trí của bạn.',
+        c = 'Bạn không đủ tiền.',
+        d = 'Có cứu thương trong server. Bạn không thể gọi BOT'
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game {'gta5'}
+
+author 'Phaos ¦ soahP#9050'
+
+description 'AI Cứu Thương'
+
+version '1.0.0'
+
+client_scripts {
+	'config.lua',
+	'client/client.lua',
+}
+
+server_scripts {
+	'config.lua',
+	'server/server.lua',
+}
